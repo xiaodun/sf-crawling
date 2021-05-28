c6e21a303cb433afb7fc1fab4f96658d13ac3673
Xiao dun <1947147481@qq.com>
2021-05-28 23:06:00 +0800

    优化下载

--- template/jpm/jpm.js
+++ template/jpm/jpm.js
@@ -4,13 +4,10 @@ const querystring = require("querystring");
 const fs = require("fs");
 
 (async () => {
-  const [reqUrl, search] =
-    "http://mp.weixin.qq.com/mp/homepage?__biz=Mzg5MTE0OTgxMA==&hid=3&sn=05d74ba6405bd21684c2f27ac3d305aa&scene=18&cid=3&begin=0&count=5&action=appmsg_list&f=json&r=0.3611643210315847&appmsg_token=".split(
-      "?"
-    );
+  const [reqUrl, search] = "".split("?");
   let params = Object.assign({}, querystring.parse(search), {
-    begin: 20,
-    count: 10,
+    begin: 0,
+    count: 30,
   });
   const audioRsp = await axios.post(
     reqUrl + "?" + querystring.stringify(params)
@@ -21,12 +18,14 @@ const fs = require("fs");
     url: item.link,
   }));
   for (let i = 0; i < audioInfoList.length; i++) {
+    const audioItem = audioInfoList[i];
     try {
       const browser = await puppeteer.launch();
-      const audioItem = audioInfoList[i];
 
-      const page = await browser.newPage();
-      await page.goto(audioItem.url);
+      const page = await browser.newPage({});
+      await page.goto(audioItem.url, {
+        waitUntil: "networkidle0",
+      });
       page.waitForSelector(".audio_card_switch");
       await page.click(".audio_card_switch");
 
@@ -38,11 +37,13 @@ const fs = require("fs");
       });
       fs.writeFileSync(`download/${audioItem.name}.mp3`, audioContent.data);
       console.log(
-        `${i + 1}/${audioInfoList.length}  ${audioItem.name} : ${downloadSrc}`
+        `${i}/${audioInfoList.length - 1}  ${audioItem.name} : ${downloadSrc}`
       );
       await browser.close();
     } catch (error) {
-      console.log("下载失败:" + audioItem.name + "  " + audioItem.url);
+      console.log(
+        "下载失败:" + i + " " + audioItem.name + "  " + audioItem.url
+      );
     }
   }
 })();
